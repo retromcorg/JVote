15ccb31968472e923641da049283f13170400fc6
Garsooon <carson12345678910@hotmail.com>
2025-06-10 03:58:47 -0700

    Adds join check

---
+++ src/main/java/com/wkaye/jvote/VoteJoinListener.java
@@ -0,0 +1,28 @@
+package com.wkaye.jvote;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
+
+public class VoteJoinListener implements Listener {
+
+    private final JVoteCommand voteCommand;
+
+    public VoteJoinListener(JVoteCommand voteCommand) {
+        this.voteCommand = voteCommand;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (voteCommand.isVoteActive()) {
+            player.sendMessage(JVoteUtils.printMessage("§aA vote for "
+                    + JVoteUtils.formatColor(voteCommand.getCurrentVoteType().color())
+                    + JVoteUtils.formatColor(voteCommand.getCurrentVoteType().toString().toLowerCase())
+                    + "§a is currently active!"));
+            player.sendMessage(JVoteUtils.printMessage("Type §b/vote yes§a or §b/vote no§a to participate."));
+        }
+    }
+}
